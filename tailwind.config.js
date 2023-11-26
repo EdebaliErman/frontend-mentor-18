07147a179e3e4912f43e71add4e9e928a445d545
EdebaliErman <edebalierman@gmail.com>
2023-11-26 17:05:15 +0300

    Tailwind config'e yazı renk ekran eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkBlue: "hsl(243, 87%, 12%)",
+        desaturedBlue: "hsl(238, 22%, 44%)",
+        brightBlue: "hsl(224, 93%, 58%)",
+        moderateCyan: "hsl(170, 45%, 43%)",
+        lightGrayishBlue: "hsl(240, 75%, 98%)",
+        lightGray: "hsl(0, 0%, 75%)",
+      },
+      fontFamily:{
+        "OpenSans":['Open Sans', "sans-serif"],
+        "Raleway":['Raleway', "sans-serif"],
+
+      }
+    },
+    screens: {
+      '2xl': { 'max': '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': { 'max': '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': { 'max': '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      'md': { 'max': '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      'sm': { 'max': '639px' },
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
